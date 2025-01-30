69469380906f2730217ad4b04d7a4395031adc22
jensclerebout <jens.clerebout@gmail.com>
2025-01-30 14:27:40 +0100

    basic functionality

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        src="/src/images/chef-claude-icon.png"
+        alt="Chef Claude icon"
+        className="header__icon"
+      />
+      <h1 className="header__title">Chef Claude</h1>
+    </header>
+  );
+}
