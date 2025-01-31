fd259c8893f958c26a962e54e2b180d53272c18d
jensclerebout <jens.clerebout@gmail.com>
2025-01-31 15:25:41 +0100

    finished Api setup

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react';
-import ClaudeRecipe from './ClaudeRecipe';
+import Recipe from './Recipe';
 import IngredientsList from './IngredientsList';
+import { getRecipeFromMistral } from '../ai';
 
 export default function Main() {
-  const [ingredients, setIngredients] = useState([
-    'all the main spices',
-    'pasta',
-    'ground beef',
-    'tomato paste',
-  ]);
-  const [recipeShown, setRecipeShown] = useState(false);
+  const [ingredients, setIngredients] = useState([]);
+  const [recipe, setRecipe] = useState('');
 
-  function toggleRecipeShown() {
-    setRecipeShown((prevRecipeShown) => !prevRecipeShown);
+  async function getRecipe() {
+    const recipeMarkdown = await getRecipeFromMistral(ingredients);
+    setRecipe(recipeMarkdown);
+    console.log(recipeMarkdown);
   }
 
   function addIngredient(formData) {
@@ -33,13 +31,10 @@ export default function Main() {
         <button className="add-ingredient-form__button">Add ingredient</button>
       </form>
       {ingredients.length > 0 && (
-        <IngredientsList
-          ingredients={ingredients}
-          toggleRecipeShown={toggleRecipeShown}
-        />
+        <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
       )}
 
-      {recipeShown && <ClaudeRecipe />}
+      {recipe && <Recipe recipe={recipe} />}
     </main>
   );
 }
